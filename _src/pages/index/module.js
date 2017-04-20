21368524260538bd4585157ac1d6bdd4a062f5d5
maxinakenty <mgreen097@gmail.com>
2017-04-20 18:33:45 +0300

    first commit

---
+++ _src/pages/index/module.js
@@ -0,0 +1,5 @@
+const module = () => {
+  console.log('hello from module!');
+};
+
+export default module;
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const webpack = require('webpack');
+const HappyPack = require('happypack');
+const IS_DEVELOPMENT = !process.env.NODE_ENV || process.env.NODE_ENV === 'development'; // Changing environment
+
+module.exports = {
+  context: __dirname + '/_src/pages/index',
+  entry: {
+    main: './index'
+  },
+  output: {
+    path: __dirname + '/_dist/js',
+    filename: '[name].js'
+  },
+
+  watch: IS_DEVELOPMENT,
+  watchOptions: {
+    argregateTimeout: 100
+  },
+
+  devtool: IS_DEVELOPMENT ? 'cheap-module-inline-source-map' : false,
+
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+  ],
+  module: {
+    rules: [{
+      test: /\.js?$/,
+      exclude: [/node_modules/],
+      loader: 'babel-loader',
+      query: {
+        presets: ['env']
+      }
+    }]
+  }
+};
+
+if (!IS_DEVELOPMENT) {
+  module.exports.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: true,
+        unsafe: true
+      }
+    }));
+}
