18f60b8d7ccee7c720e505b668296982234fcaeb
maxinakenty <mgreen097@gmail.com>
2017-05-03 19:14:36 +0300

    fixed toggle-button.js

--- _src/_js/toggle-button.js
+++ _src/_js/toggle-button.js
@@ -1,29 +1,81 @@
 class ToggleButton {
   constructor(options) {
+    this._options = options || {};
+    this._options.className = this._options.className || 'toggle-button';
+    this._options.id = this._options.id || 'toggle-button';
     this._elem;
-    this._getElem = this.getElem;
+    this._state;
+  }
+
+  // Public
+  init() {
+    this._render();
   }
 
   getElem() {
-    if(!this._elem) this._render();
+    if (!this._elem) this._render();
+
     return this._elem;
   }
 
+  getState() {
+    return this._state;
+  }
+
+  // Private
   _render() {
     this._elem = document.createElement('button');
-    this._elem.className = 'toggle-button';
+    this._elem.className = this._options.className;
+    this._elem.id = this._options.id;
+    this._elem.setAttribute('data-target', 'off');
+    this._elem.textContent = 'rhythm off';
+
+    const body = document.body;
+    body.insertBefore(this._elem, body.firstChild);
 
     this._elem.onmousedown = () => {
       return false;
     };
 
-    this._elem.addEventListener('click', function(event) {
+    this._elem.addEventListener('click', event => {
       if (event.target.closest('.toggle-button')) this._toggle();
     });
+
+    // document.addEventListener('keydown', event => {
+    //   if (event.keyCode === 'A'.charCodeAt(0) || event.keyCode === 'B'.charCodeAt(0)) {
+    //     body.removeChild(this._elem);
+    //   }
+    // });
+  }
+
+  _off() {
+    this._elem.setAttribute('data-target', 'off');
+    this._elem.textContent = 'rhythm off';
+    this._state = 'off';
+  }
+
+  _single() {
+    this._elem.setAttribute('data-target', 'single');
+    this._elem.textContent = 'rhythm single';
+    this._state = 'single';
+  }
+
+  _double() {
+    this._elem.setAttribute('data-target', 'double');
+    this._elem.textContent = 'rhythm double';
+    this._state = 'double';
   }
 
   _toggle() {
-    
+    if (this._elem.closest('[data-target="off"]')) {
+      this._single();
+
+    } else if (this._elem.closest('[data-target="single"]')) {
+      this._double();
+
+    } else {
+      this._off();
+    }
   }
 }
 
