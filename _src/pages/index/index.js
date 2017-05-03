2af998e13370be24e2f97ab1d24ea988171136f4
maxinakenty <mgreen097@gmail.com>
2017-05-03 19:18:29 +0300

    fixed index.js

--- _src/pages/index/index.js
+++ _src/pages/index/index.js
@@ -1,5 +1,13 @@
 import ToggleButton from '../../_js/Toggle-button';
+import VerticalRhythm from '../../_js/Vertical-rhythm';
 
 const toggleButton = new ToggleButton();
+const verticalRhythm = new VerticalRhythm();
 
-document.body.appendChild(toggleButton.getElem());
+
+toggleButton.init();
+verticalRhythm.init();
+
+toggleButton.getElem().addEventListener('click', () => {
+  verticalRhythm.toggleState(toggleButton.getState());
+});
