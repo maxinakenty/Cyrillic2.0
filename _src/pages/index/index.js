7f038878d957cf5d05b3bfa6286004a7a17a6780
maxinakenty <mgreen097@gmail.com>
2017-05-05 17:05:13 +0300

    added toggle-grid

--- _src/pages/index/index.js
+++ _src/pages/index/index.js
@@ -1,17 +1,18 @@
 import ToggleButton from '../../_js/Toggle-button';
 import VerticalRhythm from '../../_js/Vertical-rhythm';
+import ToggleGrid from '../../_js/Toggle-grid';
 
 const toggleButton = new ToggleButton();
 const verticalRhythm = new VerticalRhythm();
+const toggleGrid = new ToggleGrid();
 
 
 toggleButton.init();
 verticalRhythm.init();
+toggleGrid.init();
 
 toggleButton.getElem().addEventListener('click', () => {
   verticalRhythm.toggleState(toggleButton.getState());
 });
 
-document.querySelectorAll('.container').forEach(item => {
-  item.classList.add('grid-debug_show');
-});
+
