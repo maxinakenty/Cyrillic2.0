7fad322b4dad40adb397f4728fbe8d53a83c3397
maxinakenty <mgreen097@gmail.com>
2017-05-12 04:02:11 +0300

    added font-size-debug.js

--- _src/pages/index/index.js
+++ _src/pages/index/index.js
@@ -2,18 +2,22 @@ import ToggleButton from 'Toggle-button';
 import VerticalRhythm from 'Rhythm-debug';
 import GridDebug from 'Grid-debug';
 import BreakpointsDebug from 'Breakpoints-debug';
+import FontSizeDebug from '../../_js/Font-size-debug';
 
 const toggleButton = new ToggleButton();
 const verticalRhythm = new VerticalRhythm();
 const gridDebug = new GridDebug();
 const breakpointsDebug = new BreakpointsDebug();
+const fontSizeDebug = new FontSizeDebug();
 
 toggleButton.init();
 verticalRhythm.init();
 gridDebug.init();
-breakpointsDebug.init();
+fontSizeDebug.init();
+
 
 toggleButton.getElem().addEventListener('click', () => {
   verticalRhythm.toggleState(toggleButton.getState());
 });
+
 
