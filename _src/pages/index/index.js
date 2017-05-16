3677004a12a194bd510a627a38883d87de6f1043
maxinakenty <mgreen097@gmail.com>
2017-05-16 22:45:29 +0300

    fixed index.js

--- _src/pages/index/index.js
+++ _src/pages/index/index.js
@@ -3,27 +3,25 @@ import VerticalRhythm from 'Rhythm-debug';
 import GridDebug from 'Grid-debug';
 import BreakpointsDebug from 'Breakpoints-debug';
 import FontSizeDebug from 'Font-size-debug';
-import FixHeight from '../../_js/Fix-height';
+import FixHeight from 'Fix-height';
 
 const toggleButton = new ToggleButton();
 const verticalRhythm = new VerticalRhythm();
 const gridDebug = new GridDebug();
 const breakpointsDebug = new BreakpointsDebug();
 const fontSizeDebug = new FontSizeDebug();
+const fixHeight = new FixHeight();
 
 toggleButton.init();
 verticalRhythm.init();
 gridDebug.init();
-
-
+fixHeight.init();
 
 toggleButton.getElem().addEventListener('click', () => {
   verticalRhythm.toggleState(toggleButton.getState());
 });
 
 
-const fixHeight = new FixHeight();
-fixHeight.init();
 
 
 
