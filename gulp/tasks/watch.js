1f907a5a5500422ee03cc12a7588665c471efabf
maxinakenty <mgreen097@gmail.com>
2017-06-12 23:14:58 +0300

    added an arrow function

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,7 +3,7 @@
 const gulp = require('gulp');
 const __paths = require('../paths.config');
 
-gulp.task('watch', function() {
+gulp.task('watch', () => {
   gulp.watch(__paths.watch.pug, gulp.series('pug'));
   gulp.watch(__paths.watch.assets, gulp.series('assets'));
   gulp.watch(__paths.watch.styles, gulp.series('styles'));
