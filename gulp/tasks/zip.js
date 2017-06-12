e2e77a044e67fa51c34d53a611d7d8d755f044a0
maxinakenty <mgreen097@gmail.com>
2017-06-12 23:14:42 +0300

    added an arrow function

--- gulp/tasks/zip.js
+++ gulp/tasks/zip.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const zip = require('gulp-zip');
 const __paths = require('../paths.config');
 
-gulp.task('zip', function() {
+gulp.task('zip', () => {
   return gulp.src(__paths.dist.all)
     .pipe(zip('dist.zip'))
     .pipe(gulp.dest(__paths.root.dist));
