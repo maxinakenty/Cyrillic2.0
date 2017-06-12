e612704e0e1ed37349dd06561d1acb7cfd751467
maxinakenty <mgreen097@gmail.com>
2017-06-12 21:31:24 +0300

    replaced var with const

--- gulp/tasks/rev/revAssets.js
+++ gulp/tasks/rev/revAssets.js
@@ -1,17 +1,19 @@
-var gulp = require('gulp');
-var __paths = require('../../paths.config');
+'use strict';
 
-var $ = require('gulp-load-plugins')();
+const gulp = require('gulp');
+const __paths = require('../../paths.config');
+
+const $ = require('gulp-load-plugins')();
 // connect the plugin so ($. == gulp + name of plugin) gulp-if == $.if, gulp-sass == $.sass etc
 
-var combine = require('stream-combiner2').obj; // Handle errors
-var path = require('path');
-var revNapkin = require('gulp-rev-napkin');
+const combine = require('stream-combiner2').obj; // Handle errors
+const path = require('path');
+const revNapkin = require('gulp-rev-napkin');
 
-var IS_DEVELOPMENT = !process.env.NODE_ENV || process.env.NODE_ENV === 'development'; // Changing environment
+const IS_DEVELOPMENT = !process.env.NODE_ENV || process.env.NODE_ENV === 'development'; // Changing environment
 
 gulp.task('revAssets', function() {
-  var ignoreThese = '!' + path.join(__paths.root.dist, '/**/*+(css|js|html)');
+  const ignoreThese = '!' + path.join(__paths.root.dist, '/**/*+(css|js|html)');
 
   return gulp.src([
       path.join(__paths.dist.all),
