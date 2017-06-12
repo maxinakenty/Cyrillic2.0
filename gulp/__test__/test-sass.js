3ef2259109be1d6374282c1a71dd072a78d0cb8f
maxinakenty <mgreen097@gmail.com>
2017-06-12 23:10:31 +0300

    deleted __test__ folder in gulp folder

--- gulp/__test__/test-sass.js
+++
@@ -1,5 +0,0 @@
-const path = require('path');
-const sassTrue = require('sass-true');
-
-const sassFile = path.join(__dirname, 'scss', 'test.scss');
-sassTrue.runSass({file: sassFile}, describe, it);
