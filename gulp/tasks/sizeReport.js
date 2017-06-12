024c2198e17805a1ec21f4c5813b2a733fb3a831
maxinakenty <mgreen097@gmail.com>
2017-06-12 23:15:29 +0300

    added an arrow function

--- gulp/tasks/sizeReport.js
+++ gulp/tasks/sizeReport.js
@@ -4,7 +4,7 @@ const gulp = require('gulp');
 const __paths = require('../paths.config');
 const $ = require('gulp-load-plugins')();
 
-gulp.task('sizeReport', function() {
+gulp.task('sizeReport', () => {
   return gulp.src(__paths.dist.all)
     .pipe($.sizereport({
       gzip: true
