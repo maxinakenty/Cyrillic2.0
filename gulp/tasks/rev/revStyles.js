93f46840943d56eae7e273cb9668daebb9872f9a
maxinakenty <mgreen097@gmail.com>
2017-06-12 21:30:00 +0300

    replaced var with const

--- gulp/tasks/rev/revStyles.js
+++ gulp/tasks/rev/revStyles.js
@@ -1,13 +1,15 @@
-var gulp = require('gulp');
-var __paths = require('../../paths.config');
+'use strict';
 
-var $ = require('gulp-load-plugins')();
+const gulp = require('gulp');
+const __paths = require('../../paths.config');
+
+const $ = require('gulp-load-plugins')();
 // connect the plugin so ($. == gulp + name of plugin) gulp-if == $.if, gulp-sass == $.sass etc
 
-var combine = require('stream-combiner2').obj; // Handle errors
-var path = require('path');
-var revNapkin = require('gulp-rev-napkin');
-var IS_DEVELOPMENT = !process.env.NODE_ENV || process.env.NODE_ENV === 'development'; // Changing environment
+const combine = require('stream-combiner2').obj; // Handle errors
+const path = require('path');
+const revNapkin = require('gulp-rev-napkin');
+const IS_DEVELOPMENT = !process.env.NODE_ENV || process.env.NODE_ENV === 'development'; // Changing environment
 
 gulp.task('revStyles', function() {
   return gulp.src(path.join(__paths.root.dist, '/**/*.css'))
