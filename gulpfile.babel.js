3e3de1bbd41d6cc2c6734e646c5de6bb9fb82d7a
maxinakenty <mgreen097@gmail.com>
2017-06-13 06:24:08 +0300

    renamed gulpfile.js in gulpfile.babel.js

---
+++ gulpfile.babel.js
@@ -0,0 +1,36 @@
+import gulp from 'gulp';
+import requireDir from 'require-dir';
+
+requireDir('./gulp/tasks/', {
+  recurse: true
+});
+
+gulp.task('build', gulp.series(
+  'clean',
+  'sprite:png',
+  // 'sprite:svg',
+  'assets',
+  'styles',
+  gulp.parallel('imagemin', 'webpack', 'pug')
+));
+
+
+gulp.task('build:production', gulp.series(
+  'build',
+  'revAssets',
+  'revUpdateReferences',
+  'updateHtml',
+  'revStyles',
+  'updateHtml',
+  'revScripts',
+  'updateHtml',
+  // 'favicon',
+  'htmlComb',
+  'sizeReport'));
+
+
+gulp.task('demo', gulp.series('build:production', 'serve'));
+
+gulp.task('default',
+  gulp.series('build', gulp.parallel('serve', 'watch'))
+);
