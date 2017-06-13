907960cd0215c9552607ecb42a9644fa99f68c97
maxinakenty <mgreen097@gmail.com>
2017-06-13 06:29:19 +0300

    updated to es6

--- gulp/tasks/rev/updateHtml.js
+++ gulp/tasks/rev/updateHtml.js
@@ -1,13 +1,13 @@
-'use strict';
+import gulp from 'gulp';
+import __paths from '../../paths.config';
 
-const gulp = require('gulp');
-const __paths = require('../../paths.config');
+import gulpLoadPlugins from 'gulp-load-plugins';
+const $ = gulpLoadPlugins();
 
-const $ = require('gulp-load-plugins')();
-// connect the plugin so ($. == gulp + name of plugin) gulp-if == $.if, gulp-sass == $.sass etc
+import streamCombiner2 from 'stream-combiner2';
+const combine = streamCombiner2.obj;
 
-const combine = require('stream-combiner2').obj; // Handle errors
-const path = require('path');
+import path from 'path';
 const IS_DEVELOPMENT = !process.env.NODE_ENV || process.env.NODE_ENV === 'development'; // Changing environment
 
 gulp.task('updateHtml', () => {
