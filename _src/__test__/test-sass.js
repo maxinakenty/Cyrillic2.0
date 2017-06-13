fa55e15430143bcf5dc25995c898f4f657512e24
maxinakenty <mgreen097@gmail.com>
2017-06-13 05:19:51 +0300

    combed test-sass.js

--- _src/__test__/test-sass.js
+++ _src/__test__/test-sass.js
@@ -2,4 +2,7 @@ const path = require('path');
 const sassTrue = require('sass-true');
 
 const sassFile = path.join(__dirname, 'sass', 'test.scss');
-sassTrue.runSass({file: sassFile}, describe, it);
+
+sassTrue.runSass({
+  file: sassFile
+}, describe, it);
