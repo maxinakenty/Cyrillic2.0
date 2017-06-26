a6cb8c888ede6c89364f0f9b466f6604c7e0ba97
maxinakenty <mgreen097@gmail.com>
2017-06-27 02:05:31 +0300

    refactored Breakpoints-debug.js

--- _src/_js/Breakpoints-debug.js
+++ _src/_js/Breakpoints-debug.js
@@ -1,5 +1,3 @@
-const MAP = {};
-
 const defaultOptions = {
   elem: document.body,
   debugElem: 'breakpoints-debug',
@@ -13,8 +11,8 @@ export default class BreakpointsDebug {
   constructor(options) {
     this._options = this._getElem(options);
     this._elem;
-    this._map = MAP;
-    this._keydown = this._keydown.bind(this);
+    this._map = {};
+    this._handleKeyDown = this._handleKeyDown.bind(this);
   }
 
   // Public
@@ -47,24 +45,17 @@ export default class BreakpointsDebug {
   // Private
   _render() {
     this._elem = this._options.elem;
-    this._elem.addEventListener('keydown', this._keydown);
+    this._elem.addEventListener('keydown', this._handleKeyDown);
   }
 
-  _keydown(event) {
+  _handleKeyDown(event) {
     this._map[event.key] = event.type === 'keydown';
+    const showBreakKey = this._map[this._options.showBreakKey];
+    const hideBreakKey = this._map[this._options.hideBreakKey];
+    const breakKey =  this._map[this._options.breakKey];
 
-    // More productive option if (a && b) func();
-    if ([
-        this._map[this._options.showBreakKey],
-        this._map[this._options.breakKey]
-      ].every(Boolean)
-    ) this.showBreak();
-
-    if ([
-        this._map[this._options.hideBreakKey],
-        this._map[this._options.breakKey]
-      ].every(Boolean)
-    ) this.hideBreak();
+    if (showBreakKey && breakKey) this.showBreak();
+    if (hideBreakKey && breakKey) this.hideBreak();
   }
 
   _getElem(options) {
